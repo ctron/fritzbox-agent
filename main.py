91ae95cac556abc2ee404b9f1f2efbd1c0dd44a7
Jens Reimann <jreimann@redhat.com>
2020-11-26 18:46:56 +0100

    Send data

--- main.py
+++ main.py
@@ -2,8 +2,20 @@ import time
 import os
 import json
 import math
+import requests
+from urllib.parse import urljoin, urlencode, quote
 
 from fritzconnection.lib.fritzstatus import FritzStatus
+
+model_id = os.getenv('MODEL_ID', "ctron.fritzbox.status:1.0.0")
+device_id = quote(os.environ['DEVICE_ID'])
+
+endpoint = os.environ['ENDPOINT']
+path = f"/publish/{device_id}/status"
+query = "?" + urlencode(dict(model_id=model_id))
+url = urljoin(endpoint, path + query)
+
+print(url)
 
 fc = FritzStatus(password=os.environ['PASSWORD'])
 while True:
@@ -16,4 +28,7 @@ while True:
         "bytes_sent": fc.bytes_sent
     }
     print(json.dumps(status))
+    res = requests.post(url, json=status, headers={"Content-Type": "application/json"})
+    print(res)
+
     time.sleep(2)
